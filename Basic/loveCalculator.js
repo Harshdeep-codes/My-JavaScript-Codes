88000e670dcf63203b31155597730f8ef7befba6
Harshdeep Singh <90376919+Harshdeep-codes@users.noreply.github.com>
2022-08-07 09:38:21 +0530

    Create loveCalculator.js

---
+++ Basic/loveCalculator.js
@@ -0,0 +1,6 @@
+//Love Calculator input teo persons name and generate a random number in percentage
+prompt("Enter Your name");
+prompt("Enter Your Partner's name");
+var n = Math.random();
+n = Math.floor(n*100);
+alert(n+"%");
