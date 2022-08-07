aad7924a3aa45fdf9425121b3221842662904e69
Harshdeep Singh <90376919+Harshdeep-codes@users.noreply.github.com>
2022-08-07 09:36:11 +0530

    Create numBottleFromMoney.js

---
+++ Basic/numBottleFromMoney.js
@@ -0,0 +1,4 @@
+//you have to enter a amount of money you have and suppose 1 bottles price is 1.5 so  it will give you output how many bottles you can buy
+var amt = prompt("Enter the amount of money you have");
+var numBottle = Math.floor(amt / 1.5);
+alert(numBottle);
