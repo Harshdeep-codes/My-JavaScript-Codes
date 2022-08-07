a4924103cba60de2ad03b1ac8c54f610b6ed3f70
Harshdeep Singh <90376919+Harshdeep-codes@users.noreply.github.com>
2022-08-07 09:39:40 +0530

    Create searchArray.js

---
+++ Basic/searchArray.js
@@ -0,0 +1,10 @@
+var arr = ["harry", "hari", "om", "krishna"];
+console.log(arr.length);
+console.log(arr[2]);
+var name = prompt("Enter name");
+if(arr.includes(name)===true){
+alert("It is Present");
+}
+else{
+alert("It is Not Present");
+}
