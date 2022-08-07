19e9726d52f42394ff9599b85f8f04f5c7a4fb9c
Harshdeep Singh <90376919+Harshdeep-codes@users.noreply.github.com>
2022-08-07 09:36:45 +0530

    Create weekMonthDays.js

---
+++ Basic/weekMonthDays.js
@@ -0,0 +1,12 @@
+function lifeInWeeks(){
+
+  var yearsRemaining = 90 - age;
+  var days =  yearsRemaining * 365;
+  var weeks = yearsRemaining * 52;
+  var months = yearsRemaining * 12;
+
+  console.log("you have"+days+"days"+weeks+"weeks"+months+"months");
+  
+}
+
+lifeInWeeks(12);
